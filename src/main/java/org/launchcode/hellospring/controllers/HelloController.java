358e71039d9bb6cb33f458c88d4ba43e7d870e58
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2020-05-27 15:01:00 -0500

    template

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,56 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
-
-    // Handles request at path /hello
-    @GetMapping("")
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
     @ResponseBody
-    public String hello() {
-        return "Hello, Spring!";
-    }
-
-    // lives /hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye() {
-        return "Goodbye, Spring!";
-    }
-
-    // lives /hello/hello
-    // Handles request of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    private String helloWithQueryParam(@RequestParam String name) {
+    public String hello(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
-    // Handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
+    @GetMapping("hello/{name}")
     @ResponseBody
-    public String helloWithPathParam(@PathVariable String name) {
+    public String helloAgain(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
-    public static String createMessage() {
-
-        return "HI";
-
-    }
-
-
-    // lives /hello/form
     @GetMapping("form")
-
     public String helloForm() {
-        return "<html>" +
-                    "<body>" +
-                        "<form action='hello' method='post'>" + //submit a request to /hello
-                            "<input type='text' name='name'>" +
-                            "<input type='submit' value='Greet me!'>" +
-                        "</form" +
-                    "</body" +
-                "</html>";
+        return "form";
     }
-
 }
+
